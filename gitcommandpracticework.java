d586f2a270396ef7ae3bc52ea588a7689f590876
jadamsj <jadams13@luc.edu>
2024-09-10 15:19:35 -0500

    Add files via upload

---
+++ gitcommandpracticework.java
@@ -0,0 +1,9 @@
+public class App{
+	public static void main(String[] args) {
+
+	   double price = 23.00;
+	   double discount = .10;
+	   price = (1 - discount)  price;
+	   System.out.println(With discount the price is  +  price); 
+	}
+}
